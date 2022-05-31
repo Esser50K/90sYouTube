4965400ca1820ba48ccb0c9f5486d434242cf003
matedyl <6464193+matedyl@users.noreply.github.com>
2022-05-31 09:28:24 +0200

    Feature/install material UI (#1)
    * Installed material-ui components and icons
    
    * Added AppBar and Drawer placeholder for spacing purposes
    
    * Installed react-router-dom
    
    * Added routing, layout page and pages:
    - /
    - /output
    - 404
    Added stub for YoutubeVideoThumbnail component
    
    * Changed icon and placeholder in appbar url input
    
    * Added layout and sample image assets for YoutubeVideoThumbnail component
    Removed unnecessary container spacing in Layout
    Added second line of thumbnails in Home
    
    * Renamted output page to watch
    Added label to appbar
    Added incentives and donate button from homepage to sidebar
    
    * Added props to YoutubeVideoThumbnail component
    
    * Added .idea dir to gitignore
    
    * Added routing for search query: /watch?v=xyz
    
    * Added thumbnail pictures for vid grid
    Added hover state for YoutubeVideoThumbnail
    Replaced Link navigation with programmatic one in YoutubeVideoThumbnail component
    
    * Added Layout variant with collapsed drawer
    Removed unused props from Watch page
    
    * Moved Toast to bottom left part of viewport
    Moved video rendering to Watch page
    Implemented video loading: from route and url input field in appbar
    Implemented reading mobile state from ThemeProvider
    
    * Implemented hiding button when input url empty
    
    * Added and set up new font
    
    * Set up new colors
    
    * Styled convert button, input field and fixed drawer spacing
    
    * Added moneyfall gif in the sidebar
    
    * Added missing code background
    Added 404 page
    
    * Added under construction gif to index page
    
    * Added generic serif font type
    #1
    
    * Updated incorrect local font reference
    #1
    
    * Added channel name to video thumbnail
    #1
    
    * Added responsiveness
    Removed the gif background
    #1
    
    * Moved donate container to the bottom of the sidebar
    #1
    
    Co-authored-by: mad <jetbrains@mad.solares.studio>

---
+++ app/app/src/components/YoutubeVideoThumbnail.tsx
@@ -0,0 +1,85 @@
+import {createStyles, makeStyles, Theme} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import {useNavigate} from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        thumbnailContainer: {
+            cursor: 'pointer',
+            border: "4px ridge white",
+        },
+        thumbnail: {
+            backgroundSize: "cover",
+            '&::before': {
+                content: "''",
+                display: "inline-block",
+                width: "1px",
+                height: 0,
+                paddingBottom: "calc(100% / (16/9))",
+            },
+            filter: "blur(1px) saturate(300%)",
+            marginBottom: theme.spacing(1.5),
+        },
+        videoMetadata: {
+            display: 'grid',
+            gridTemplateAreas: "'avatar text'",
+            gap: theme.spacing(1),
+        },
+        metadataAvatar: {
+            gridItem: 'avatar',
+            width: '40px',
+        },
+        metadataText: {
+            gridItem: 'text',
+        },
+        channelTitle: {
+            marginTop: theme.spacing(0.5),
+        },
+    })
+)
+
+interface YoutubeVideoThumbnailProps {
+    youtubeId: string
+    title: string
+    thumbnailImage: {
+        src: string
+    }
+    avatarImage: {
+        alt: string
+        src: string
+    }
+    channel: {
+        youtubeId: string
+        title: string
+    }
+}
+
+function YoutubeVideoThumbnail({ youtubeId, title, thumbnailImage, avatarImage, channel }: YoutubeVideoThumbnailProps) {
+    const classes = useStyles();
+    const navigate = useNavigate();
+
+    const navigateToWatch = () => navigate(`/watch?v=${youtubeId}`)
+
+    return (
+        <Box className={classes.thumbnailContainer} bgcolor="background.paper" onClick={navigateToWatch}>
+            <Box className={classes.thumbnail} style={{backgroundImage: `url('${thumbnailImage.src}')`}} />
+            <Box className={classes.videoMetadata}>
+                <Box className={classes.metadataAvatar}>
+                    <Avatar {...avatarImage} />
+                </Box>
+                <Box className={classes.metadataText}>
+                    <Typography variant="subtitle1" color="textPrimary">
+                        { title }
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" className={classes.channelTitle}>
+                        { channel.title }
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default YoutubeVideoThumbnail;
---
+++ app/app/src/pages/Home.tsx
@@ -0,0 +1,166 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import YoutubeVideoThumbnail from "../components/YoutubeVideoThumbnail";
+
+function Home() {
+    const featuredVideos = [
+        {
+            id: '1',
+            props: {
+                youtubeId: 'ASJ3iY0-qpQ',
+                title: 'Turning videos into TEXT? 🔠',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/1.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        },
+        {
+            id: '2',
+            props: {
+                youtubeId: '5nnVj1i3Si4',
+                title: 'BANNED from Twitter for being offensive',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/2.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        },
+        {
+            id: '3',
+            props: {
+                youtubeId: 'hId3HtBwPKo',
+                title: 'Neighbour got Caught! 😱',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/3.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        },
+        {
+            id: '4',
+            props: {
+                youtubeId: 'CxMRcBD5sE8',
+                title: 'Detecting Faces in Thousands of Videos FAST #python',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/4.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        },
+        {
+            id: '5',
+            props: {
+                youtubeId: 'if2q0XxCWQ0',
+                title: '#Python Tips, Tricks and Quirks - Hackertalk',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/5.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        },
+        {
+            id: '6',
+            props: {
+                youtubeId: 'ASJ3iY0-qpQ',
+                title: 'Exploiting my Cat for Fun and Profit #shorts',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/6.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        },
+        {
+            id: '7',
+            props: {
+                youtubeId: 'ASJ3iY0-qpQ',
+                title: 'Smart Home Cat turns on Lights #shorts',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/7.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        },
+        {
+            id: '8',
+            props: {
+                youtubeId: 'ASJ3iY0-qpQ',
+                title: 'Saving Sushi from Doom #shorts',
+                thumbnailImage: {
+                    src: '/images/video-thumbnails/8.webp'
+                },
+                avatarImage: {
+                    alt: 'Esser50k',
+                    src: '/images/avatar/Esser50k.jpg'
+                },
+                channel: {
+                    youtubeId: 'UCBxzOQd2v9wWfiMDrf_RQ7A',
+                    title: 'Esser50k',
+                },
+            }
+        }
+    ]
+
+    return (
+        <Grid container spacing={3}>
+            {
+                featuredVideos.map(featuredVideo => (
+                    <Grid key={featuredVideo.id} item xs={12} sm={6} md={4} lg={3}>
+                        <YoutubeVideoThumbnail {...featuredVideo.props} />
+                    </Grid>
+                ))
+            }
+        </Grid>
+    )
+}
+
+export default Home;
