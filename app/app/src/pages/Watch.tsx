6ecbd9dae73c0e5be2fbd5e20b909e737f170174
matedyl <6464193+matedyl@users.noreply.github.com>
2022-09-25 16:21:47 +0200

    Watch page layout (desktop) (#19)
    * https://app.clickup.com/t/2zh2dhz
    
    * Watch page layout on mobile (#20)
    
    Added column layout varation to FeaturedVideos component for different layout on Watch page
    
    Co-authored-by: mad <jetbrains@mad.solares.studio>
    
    * adjust font adjustment to be below 1 to fit inside container
    
    Co-authored-by: mad <jetbrains@mad.solares.studio>
    Co-authored-by: André Jorge Esser <bepandre@hotmail.com>

--- app/app/src/pages/Watch.tsx
+++ app/app/src/pages/Watch.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import {useSearchParams} from "react-router-dom";
-import Container from "@material-ui/core/Container/Container";
 import Box from "@material-ui/core/Box/Box";
 import Button from "@material-ui/core/Button/Button";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
@@ -11,19 +10,44 @@ import {getUrl} from "../utils"
 import FeaturedVideos from "../components/FeaturedVideos";
 import {getRecommendations} from "../services/recommendations";
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         downloadContainer: {
             display: 'grid',
             justifyContent: 'center',
             justifyItems: 'center',
-        }
+        },
+        layout: {
+            display: 'grid',
+            gridTemplate: '"main" auto "sidebar" auto / 1fr',
+            [theme.breakpoints.up('md')]: {
+                gridTemplate: '"main sidebar" auto / 1fr 402px',
+            },
+            width: '100%',
+            maxWidth: '1340px',
+            margin: '0 auto',
+        },
+        layoutMain: {
+            gridArea: 'main',
+            minWidth: '100%',
+            [theme.breakpoints.up('md')]: {
+                minWidth: '640px',
+            },
+        },
+        layoutSidebar: {
+            gridArea: 'sidebar',
+            maxWidth: '100%',
+            [theme.breakpoints.up('md')]: {
+                maxWidth: '402px',
+            },
+            width: '100%',
+        },
     })
 )
 
 function Watch() {
     const [searchParams] = useSearchParams();
-    const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+    const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
     const [playerContent, setPlayerContent] = useState("")
     const [muted, setMuted] = useState(false)
     const [image, setImage] = useState()
@@ -100,24 +124,28 @@ function Watch() {
 
     return (
         <>
-            <Container maxWidth="md">
-                <AsciiPlayer
-                    ytURL={getYTUrl()}
-                    image={image}
-                    show={true}
-                    onRepaint={setPlayerContent}
-                    isMobile={isMobile}
-                />
-                {
-                    playerContent &&
-                    <Box className={classes.downloadContainer}>
-                        <Button variant="outlined" disabled={downloading} onClick={downloadImage}>
-                            {downloading ? 'Downloading...' : 'Download image'}
-                        </Button>
-                    </Box>
-                }
-            </Container>
-            <FeaturedVideos videos={featuredVideos}/>
+            <div className={classes.layout}>
+                <div className={classes.layoutMain}>
+                    <AsciiPlayer
+                        ytURL={getYTUrl()}
+                        image={image}
+                        show={true}
+                        onRepaint={setPlayerContent}
+                        isMobile={isMobile}
+                    />
+                    {
+                        playerContent &&
+                        <Box className={classes.downloadContainer}>
+                            <Button variant="outlined" disabled={downloading} onClick={downloadImage}>
+                                {downloading ? 'Downloading...' : 'Download image'}
+                            </Button>
+                        </Box>
+                    }
+                </div>
+                <div className={classes.layoutSidebar}>
+                    <FeaturedVideos videos={featuredVideos} columnLayout={!isMobile} />
+                </div>
+            </div>
             {muted ? <Toast text={(isMobile ? "tap" : "click") + " to unmute"}/> : null}
         </>
     )
